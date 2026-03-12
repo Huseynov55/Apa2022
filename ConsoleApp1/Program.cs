1aea713e2d18fb3b6539428e2a4e10d044751e92
Huseynov55 <anarchh-apa202@code.edu.az>
2026-03-12 19:27:23 +0400

    task5

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int n=Convert.ToInt32(Console.ReadLine());
+            //Digit(n);
+            //string s=Console.ReadLine();
+            //Sait(s);
+            //int[] n= [15, 34, 65, 54, 12, 65, 7, 5];
+            //SumNum(n);
+            int[] arr = [13, 21, 2, 6, 5, 8];
+            Index(arr);
+
+        }
+        public static void Index( int[] n)
+        {
+            int index = -1;
+            for (int i = 0; i < n.Length; i++)
+            {
+                if (n[i] == 13)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            if (index == -1)
+            {
+                Console.WriteLine("not found");
+            }
+            else
+            {
+                Console.WriteLine(index);
+            }
+            
+        }
+
+
+
+
+        public static void SumNum(int[] n)
+        {
+            int sum = 0;
+            for(int i = 0;i < n.Length; i++)
+            {
+                for(int j = 2; j < n[i]; j++)
+                {
+                    if(n[i]%j == 0)
+                    {
+                        sum += n[i];
+                        break;
+                    }    
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+
+
+        public static void Digit(int n)
+        {
+            int count = 1;
+            while (n>9)
+            {
+                n /= 10;
+                count++;
+            }
+            Console.WriteLine(count);
+        }
+
+
+
+        public static void Sait(string s)
+        {
+            char[] saitler = ['a', 'i', 'e', 'o', 'u'];
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                
+                for (int j = 0; j < saitler.Length; j++)
+                {
+                    if (s[i] == saitler[j])
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,139 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            //int number = 55;
+            //Nums(number);
+            //2
+            //int x=Convert.ToInt32(Console.ReadLine()); 
+            //int y=Convert.ToInt32(Console.ReadLine());
+            //Pow(x, y);
+            //3
+            //int[] nums = [1, 5, 34, 65, 76, 85, 94, 25];
+            //SumNums(nums);
+            //4
+            //int[] arrnums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+            //Max(arrnums);
+            //5
+            //int x= Convert.ToInt32(Console.ReadLine());
+            //int y= Convert.ToInt32(Console.ReadLine());
+            //BigNum(x, y);
+            //6
+            //string[] s=["salam", "anar", "hesen"];
+            //Combine(s);
+            //7
+            //SumNumber(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+            //8
+
+
+
+
+        }
+        public static void BigNum1(int x, int y, int z)
+        {
+            int[] arr= {x, y, z};
+            int max=arr[0];
+            int min=arr[0];
+            int avg=arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if ( )
+                {
+
+                }
+            }
+        }
+
+
+
+
+        public static void SumNumber(params int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                sum+= nums[i];
+            }
+            Console.WriteLine(sum);
+        }
+
+
+
+
+        public static void Combine(string[] s)
+        {
+            string con = "";
+            for (int i = 0; i < s.Length; i++)
+            {
+                con += s[i]+" ";
+            }
+            Console.WriteLine(con);
+        }
+
+
+
+        public static void BigNum(int x, int y)
+        {
+            
+            if (x > y)
+            {
+                x *= 10;
+                x += y;
+                Console.WriteLine(x);
+            }
+            else
+            {
+                y *= 10;
+                y += x;
+                Console.WriteLine(y);
+            }
+        }
+
+        public static void Max(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 0;i < arr.Length; i++)
+            {
+                if(max < arr[i])
+                {
+                    max= arr[i];
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+        public static void SumNums(int[] nums)
+        {
+            int sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 5 == 0)
+                {
+                    sum += nums[i];
+                }
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static void Pow(int x, int y)
+        {
+           double m=Math.Pow(x, y);
+            Console.WriteLine(m);
+        }
+        public static void Nums(int n)
+        {
+            int count = 0;
+            for (int i = 10; i <= n; i++)
+            {
+                if (i < 100)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
